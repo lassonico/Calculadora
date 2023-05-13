29a467de34ec8fcdae6b727e964ea9c70ed9ec1b
lassonico <sionvmx@gmail.com>
2023-05-13 09:58:15 -0500

    Ajuste de colores

--- src/components/Boton.jsx
+++ src/components/Boton.jsx
@@ -1,7 +1,7 @@
 
 function Boton({value, handler}) {
   return (
-    <div className={`cursor-pointer p-5 w-[23%] ${value == "D" ? 'bg-red-500 text-white hover:bg-red-600' : ''} ${value == "C" || value == "=" ? 'bg-orange-400 w-[47%] text-white hover:bg-orange-500' : '' }  rounded-xl text-center font-bold text-cyan-800 shadow-lg select-none text-xl bg-slate-200 hover:bg-slate-300`} onClick={() => handler(value)}>
+    <div className={`cursor-pointer p-5 w-[23%] ${value == "D" ? 'bg-red-500 text-white hover:bg-red-600' : 'bg-slate-200 hover:bg-slate-300'} ${value == "C" || value == "=" ? 'bg-orange-400 w-[47%] text-white hover:bg-orange-500' : 'bg-slate-200 hover:bg-slate-300' }  rounded-xl text-center font-bold text-cyan-800 shadow-lg select-none text-xl`} onClick={() => handler}>
         {value}
     </div>
   )
